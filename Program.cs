e6dbec616aa004315c1c67cca2ec15bb23da4e48
Family <Family@DESKTOP-DUQIOF8>
2025-10-12 18:03:56 -0500

    Tareas en program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
 ﻿using ImageMetadataTools.UI;
 
 MenuPrincipal menu = new();
-menu.Inicio();               
+menu.Inicio();
+
+/*
+ *Organizar la clase Saver para guaradar en un txt, guarda como jpg.
+ *Crear archivo txt en donde está la imagen.
+ *Buscar en carpeta.
+ *mostrar cantidad de imagenes.
+ *mostrar en pantalla los nombres. 
+ *usuario elige una.
+ *
+ */
