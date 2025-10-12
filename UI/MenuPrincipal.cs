f491d28c1ef9e52e1117e3371374a6ef57d29dea
Family <Family@DESKTOP-DUQIOF8>
2025-10-12 17:49:01 -0500

    Limpieza de código

--- UI/MenuPrincipal.cs
+++ UI/MenuPrincipal.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ImageMetadataTools.Models;
+﻿using ImageMetadataTools.Models;
 using ImageMetadataTools.Services;
 
 namespace ImageMetadataTools.UI
