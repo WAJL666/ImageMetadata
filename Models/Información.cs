cf15c9e3111c39e3f5df712ebb9d53d6589743c3
Family <Family@DESKTOP-DUQIOF8>
2025-11-04 16:17:40 -0500

    Clase comentarios

---
+++ Models/Información.cs
@@ -0,0 +1,58 @@
+﻿
+/* 
+ Clases y sus Metodos
+
+ * Clase Style              
+ *      mostrarMenu
+ *      MostrarMenuAgrupar
+ *      MostrarMenuNavegar
+ *      LeerOpcion
+ *      MostrarLinea
+ *      MostrarTitulo
+ *      MostrarError
+ *      MostrarComentarios
+ *      MostrarContenido
+ *      MostrarListado
+ *      limpiarPantalla
+ *      
+ * Clase Manage
+ *      MostrarContenidoCarpeta
+ *      NavegarASubcarpeta
+ *      NavegarAtras
+ *      NavegarAdelante
+ *      EsRutaValida
+ *      ObtenerImagenesValidas
+ *      ObtenerSubcarpetas
+ *           
+ *      
+ * Clase MetadataReader    
+ *      GetMetadata
+ *      InicializarMetadata
+ *      ExtraerExif
+ *      GetImageFormat
+ *      GetString
+ *      GetRationalString
+ *      GetShort
+ *      GetGPS
+ *      
+ * Clase MetadataSaver
+ *      GuardarEnArchivo
+ *      GenerarTexto
+ *      ObtenerRutaDestino
+ *      
+ * Clase MenuPrincipal
+ *      Inicio
+ *      ProcesarOpcion
+ *      IniciarNavegacion
+ *      PedirRuta
+ *      
+ * Clase OperMenu
+        VolverAlMenu
+ *      ProcesarImagen
+ *      GuardarInformacion
+ *      ProcesaImage
+ *      EjecutarAgrupacion
+ *      ValidarArchivo
+ *      MostrarMetadatos
+ *       
+ */
